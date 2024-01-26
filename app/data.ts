18bfb74661aa110db2932253e11280a0a1e21343
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2024-01-26 11:18:49 -0500

    Created data.ts file and added example data

---
+++ app/data.ts
@@ -0,0 +1,150 @@
+export const data1 = {
+    nameOfRoute: "Example of Sandbagging",
+    indoorOutdoor: "Indoor",
+    type: "Boulder",
+    send: true,
+    grade: "v4",
+    myGrade: "v6",
+    enjoyment: 3,
+    date: "2021-08-01",
+    time: "12:00",
+    location: "Ascend SouthSide",
+    weather: null,
+    notes: "This was sandbagged as shit",
+};
+
+export const data2 = {
+    nameOfRoute: "Crimson Crack",
+    indoorOutdoor: "Outdoor",
+    type: "Trad",
+    send: false,
+    grade: "5.10",
+    myGrade: "5.9",
+    enjoyment: 4,
+    date: "2021-09-15",
+    time: "10:30",
+    location: "Red Rock Canyon",
+    weather: "Sunny",
+    notes: "Fantastic hand jams!",
+};
+
+export const data3 = {
+    nameOfRoute: "Slabzilla",
+    indoorOutdoor: "Indoor",
+    type: "Top-rope",
+    send: true,
+    grade: "5.8",
+    myGrade: "5.10",
+    enjoyment: 2,
+    date: "2021-07-20",
+    time: "14:45",
+    location: "Vertical Adventures",
+    weather: null,
+    notes: "Slippery holds!",
+};
+
+export const data4 = {
+    nameOfRoute: "Whipper Wall",
+    indoorOutdoor: "Outdoor",
+    type: "Sport",
+    send: false,
+    grade: "5.12a",
+    myGrade: "5.11b",
+    enjoyment: 5,
+    date: "2021-10-05",
+    time: "11:15",
+    location: "Smith Rock",
+    weather: "Partly cloudy",
+    notes: "Took a massive whipper but loved it!",
+};
+
+export const data5 = {
+    nameOfRoute: "Vertigo",
+    indoorOutdoor: "Indoor",
+    type: "Lead",
+    send: true,
+    grade: "5.11c",
+    myGrade: "5.11a",
+    enjoyment: 4,
+    date: "2021-11-12",
+    time: "18:00",
+    location: "Vertical Limit Gym",
+    weather: null,
+    notes: "Pumped out at the crux!",
+};
+
+export const data6 = {
+    nameOfRoute: "Granite Groove",
+    indoorOutdoor: "Outdoor",
+    type: "Trad",
+    send: true,
+    grade: "5.9",
+    myGrade: "5.8",
+    enjoyment: 5,
+    date: "2021-06-25",
+    time: "09:30",
+    location: "Yosemite National Park",
+    weather: "Clear skies",
+    notes: "Beautiful crack climbing!",
+};
+
+export const data7 = {
+    nameOfRoute: "Overhang Odyssey",
+    indoorOutdoor: "Indoor",
+    type: "Boulder",
+    send: false,
+    grade: "V7",
+    myGrade: "V6",
+    enjoyment: 3,
+    date: "2021-12-03",
+    time: "13:45",
+    location: "ClimbOn Gym",
+    weather: null,
+    notes: "Struggled with the overhang!",
+};
+
+export const data8 = {
+    nameOfRoute: "Limestone Labyrinth",
+    indoorOutdoor: "Outdoor",
+    type: "Sport",
+    send: true,
+    grade: "5.10b",
+    myGrade: "5.10a",
+    enjoyment: 4,
+    date: "2021-09-08",
+    time: "10:00",
+    location: "Kalymnos, Greece",
+    weather: "Sunny",
+    notes: "Stunning views from the top!",
+};
+
+export const data9 = {
+    nameOfRoute: "Sloper Surprise",
+    indoorOutdoor: "Indoor",
+    type: "Top-rope",
+    send: false,
+    grade: "5.7",
+    myGrade: "5.6",
+    enjoyment: 2,
+    date: "2021-07-30",
+    time: "16:20",
+    location: "RockCity Gym",
+    weather: null,
+    notes: "Slippery slopers!",
+};
+
+export const data10 = {
+    nameOfRoute: "Sandstone Shuffle",
+    indoorOutdoor: "Outdoor",
+    type: "Trad",
+    send: true,
+    grade: "5.8",
+    myGrade: "5.9",
+    enjoyment: 5,
+    date: "2022-01-18",
+    time: "11:30",
+    location: "Joshua Tree National Park",
+    weather: "Mild",
+    notes: "Enjoyed the varied crack sizes!",
+};
+
